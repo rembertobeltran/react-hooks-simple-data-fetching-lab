99cdfbe4def19cb76722dafd44c7279591dba345
Remberto <rembertobeltran@icloud.com>
2022-06-10 16:35:20 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect} from "react";
+
+function App() {
+    const [imageSet, setImage] = useState([]);
+    const [loadedSet, setLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then((resp) => resp.json())
+            .then((image) => {
+                setImage(image);
+                setLoaded(true);
+            });
+    }, []);
+
+    if (!loadedSet) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <img src={imageSet.message} alt="A Random Dog"></img>
+        </div>
+    );
+}
+
+export default App;
